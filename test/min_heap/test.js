e1189b9aeae8450edc9e82a3f53d869d849816d7
iamaleko <a@da.am>
2024-04-28 23:33:13 +0200

    Add test sample

---
+++ test/min_heap/test.js
@@ -0,0 +1,9 @@
+import assert from 'assert';
+
+// describe('Class', () => {
+//   describe('.method', () => {
+//     it('should return ? when ?', () => {
+//       assert.equal(/* what */, /* to what */);
+//     });
+//   });
+// })
